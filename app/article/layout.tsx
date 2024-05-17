c285d6399e18915a27dd36d447115597f6cd7533
jay <rikodwijaya3@gmail.com>
2024-05-17 16:45:34 +0700

    header

--- app/article/layout.tsx
+++ app/article/layout.tsx
@@ -6,9 +6,24 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       <body className="bg-slate-600">
         <div className="flex flex-col min-h-screen">
           {/* Header */}
-          <header className="p-4 bg-slate-800 w-full h-[72px] flex items-center justify-center">
-            <h1 className="text-white text-2xl">Header</h1>
+          <header className="p-4 bg-slate-800 w-full h-[72px] flex items-center justify-between">
+            {/* Bagian kiri header */}
+            <div className="flex items-center">
+              <h1 className="text-white text-right">Header</h1>
+            </div>
+
+            {/* Bagian kanan header */}
+            <div className="flex items-center">
+              <span className="text-white mr-4 cursor-pointer">INI</span>
+              <span className="text-white mr-4 cursor-pointer">APA</span>
+              <span className="text-white mr-4 cursor-pointer">INIS</span>
+              <span className="text-white mr-4 cursor-pointer">APAS</span>
+              <button className="bg-sky-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Button
+              </button>
+            </div>
           </header>
+
           {/* Main Content */}
           <main className="flex-grow p-4 bg-white-500 ">{children}</main>
 
