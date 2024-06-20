d1b8fc94d949161eaa28a33d91d2a52156b503bf
jay <rikodwijaya3@gmail.com>
2024-06-20 16:15:55 +0700

    fix-bytopic

--- app/bytopic/page.tsx
+++ app/bytopic/page.tsx
@@ -14,19 +14,19 @@ export default function page() {
         </div>
       </div>
       {/* Bg2 */}
-      <div className="bg-yellow-400 ">
+      <div className="bg-white ">
         {/* Wrap Cotent px */}
-        <div className="bg-green-800 mx-[162px] h-full">
+        <div className=" mx-[162px] h-full">
           {/* Wrap Cotent px */}
-          <div className="bg-red-600 flex justify-between pt-[80px] pb-[24px]">
+          <div className=" flex justify-between pt-[80px] pb-[24px]">
             <h1 className="text-black font-bold text-[40px] ">
               Recent Updates
             </h1>
-            <div>View All</div>
+            <div className="text-[#9B9B9B]">View All</div>
           </div>
           {/* gambar kiri */}
-          <div className="bg-purple-600 flex flex-row gap-[24px]">
-            <div className="bg-red-950 w-1/2 flex flex-col gap-[16px] text-black">
+          <div className=" flex flex-row gap-[24px]">
+            <div className="w-1/2 flex flex-col gap-[16px] text-black">
               <Image
                 src="/pp.webp"
                 alt="gambar"
@@ -43,31 +43,51 @@ export default function page() {
                 Alzheimer’s symptoms.
               </p>
             </div>
-            {/* Wrap gambar kanan */}
-            <div className="bg-green-700 w-1/2 flex flex-row gap-2">
-              <div className="bg-red-700 w-1/3">
+            <div className="bg-red-300 w-1/2 gap-2">
+              <div className="bg-yellow-300 flex flex-row gap-2">
                 <img
                   src="/Rectangle.jpg"
                   alt="gambar"
-                  className="w-[84px] lg:w-full h-[84px] lg:h-[155px] rounded-2xl object-cover overflow-hidden"
+                  className="w-auto h-[148px] rounded-2xl object-cover overflow-hidden"
                   style={{ aspectRatio: "466 / 292" }}
                 />
+                <div className="bg-green-300">
+                  <p className="text-[#9B9B9B] text-[10px] lg:text-[14px]">
+                    May 8, 2023
+                  </p>
+                  <p className="text-[16px] lg:text-[20px] text-black font-bold">
+                    The Benefits of Music for People with Alzheimer’s
+                  </p>
+                  <p className="text-[10px] lg:text-[16px] text-black">
+                    Music therapy and other musical Activities may help reduce
+                    Alzheimer’s symptomps.
+                  </p>
+                </div>
               </div>
-              <div className="bg-gray-700 w-2/3 gap-[8px]">
-                <p className="text-[#9B9B9B] text-[10px] lg:text-[14px]">
-                  May 8, 2023
-                </p>
-                <p className="text-[16px] lg:text-[20px] text-black font-bold">
-                  The Benefits of Music for People with Alzheimer’s
-                </p>
-                <p className="text-[10px] lg:text-[16px] text-black">
-                  Music therapy and other musical Activities may help reduce
-                  Alzheimer’s symptomps.
-                </p>
+              <div className="bg-yellow-300 flex flex-row gap-2 mt-[24px]">
+                <img
+                  src="/Rectangle.jpg"
+                  alt="gambar"
+                  className="w-auto h-[148px] rounded-2xl object-cover overflow-hidden"
+                  style={{ aspectRatio: "466 / 292" }}
+                />
+                <div className="bg-green-300">
+                  <p className="text-[#9B9B9B] text-[10px] lg:text-[14px]">
+                    May 8, 2023
+                  </p>
+                  <p className="text-[16px] lg:text-[20px] text-black font-bold">
+                    The Benefits of Music for People with Alzheimer’s
+                  </p>
+                  <p className="text-[10px] lg:text-[16px] text-black">
+                    Music therapy and other musical Activities may help reduce
+                    Alzheimer’s symptomps.
+                  </p>
+                </div>
               </div>
             </div>
+            {/* Wrap gambar kanan */}
           </div>
-          <div className="bg-zinc-800 flex justify-between">
+          <div className="bg-white flex justify-between">
             <h1 className="text-black font-bold text-[40px]">Editors’ Picks</h1>
             <div>View All</div>
           </div>
